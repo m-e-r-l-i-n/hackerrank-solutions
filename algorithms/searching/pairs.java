6c4484592ed1c5b2665d6009032ba81f89ac394c
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-23 14:58:39 +0530

    Create pairs.java

---
+++ algorithms/searching/pairs.java
@@ -0,0 +1,52 @@
+problem link-https://www.hackerrank.com/challenges/pairs/problem?h_r=internal-search
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        int i,l=arr.length,j,c=0;
+        Arrays.sort(arr);
+        for(i=0;i<l;i++)
+        if(Arrays.binarySearch(arr,arr[i]+k) >=0 ) c++;
+        return c;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
