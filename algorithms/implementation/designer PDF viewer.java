9fc78e6371591fe616e89c7e592129a884d5272c
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-25 02:33:48 +0530

    Create designer PDF viewer.java

---
+++ algorithms/implementation/designer PDF viewer.java
@@ -0,0 +1,47 @@
+problem link-https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] h, String s) {
+        int i,l=s.length(),ht=0;   //b=1;
+        for(i=0;i<l;i++)
+        if(h[s.charAt(i)-97]>ht) ht=h[s.charAt(i)-97];  //note that ht of a is at 0, b 1...
+        return l*ht;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] h = new int[26];
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
+        }
+
+        String word = scanner.nextLine();
+
+        int result = designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
