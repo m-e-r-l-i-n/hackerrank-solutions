ea4f59d30c32409b2079c36f06aa15bf7635c0db
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-25 03:02:21 +0530

    Create sparse arrays.java

---
+++ data structures/arrays/sparse arrays.java
@@ -0,0 +1,68 @@
+problem link-https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the matchingStrings function below.
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        HashMap<String,Integer> w=new HashMap<>();
+        int i;
+        for(i=0;i<strings.length;i++)
+        if(w.get(strings[i])==null) w.put(strings[i],1);
+        else w.put(strings[i],w.get(strings[i])+1);
+
+        int ans[]=new int[queries.length];
+        for(i=0;i<queries.length;i++)
+        if(w.get(queries[i])!=null) ans[i]=w.get(queries[i]);
+        return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
