f7015fa614c6fc2c4029d6dcdbf5732bea460ad5
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-26 14:37:29 +0530

    Create gaming array.java

---
+++ algorithms/constructive algorithms/gaming array.java
@@ -0,0 +1,62 @@
+problem link-https://www.hackerrank.com/challenges/an-interesting-game-1/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gamingArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static String gamingArray(List<Integer> arr) {
+    // Write your code here
+        int n=arr.size(),i,max=arr.get(0),c=0;
+        for(i=1;i<arr.size();i++)
+        if(arr.get(i)>max) {max=arr.get(i); c++;}    //simple observation skill
+        if(c%2==1) return "ANDY";
+        else return "BOB";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, g).forEach(gItr -> {
+            try {
+                int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.gamingArray(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
