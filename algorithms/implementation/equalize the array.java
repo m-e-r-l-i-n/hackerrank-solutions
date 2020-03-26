a0e2d7fd7deb89a3c90a3cf07d6fd1aa2a455b36
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-26 13:12:54 +0530

    Create equalize the array.java

---
+++ algorithms/implementation/equalize the array.java
@@ -0,0 +1,51 @@
+problem link-https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        int i,n=arr.length,a[]=new int[101],max=0;
+        for(i=0;i<n;i++)
+        a[arr[i]]++;
+        for(i=0;i<101;i++)
+        if(a[i]>max) max=a[i];
+
+        return n-max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
