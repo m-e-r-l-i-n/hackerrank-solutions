645d5c9f8f07f6822b58153f4036e65cea0b1d42
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-03-26 12:47:57 +0530

    Create picking numbers.java

---
+++ algorithms/implementation/picking numbers.java
@@ -0,0 +1,55 @@
+problem link-https://www.hackerrank.com/challenges/picking-numbers/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+    // Write your code here
+    int c[]=new int[100],i,l=0;
+    for(i=0;i<a.size();i++)
+    c[a.get(i)]++;
+
+    for(i=1;i<100;i++)
+    l=l>c[i]+c[i-1]?l:c[i]+c[i-1];
+    return l;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
