1ce0bff158a8020d633054bd72f5da2b107a38c5
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-05-14 13:43:15 +0530

    Create 4.python:division.py

---
+++ language proficiency/python/4.python:division.py
@@ -0,0 +1,7 @@
+problem link-https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print((int)(a/b))
+    print(a/b)
