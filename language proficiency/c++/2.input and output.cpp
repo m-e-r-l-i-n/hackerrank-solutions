23939677cb9519fbf5ce8ccd82bebdce0be57a17
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-05-14 13:58:29 +0530

    Create 2.input and output.cpp

---
+++ language proficiency/c++/2.input and output.cpp
@@ -0,0 +1,18 @@
+problem link-https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<(a+b+c);
+    return 0;
+}
+
