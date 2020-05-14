3865852e190f49fedf2f2a7adbc989ecb0675cf6
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-05-14 13:48:48 +0530

    Create 11.finding the percentage.py

---
+++ language proficiency/python/11.finding the percentage.py
@@ -0,0 +1,12 @@
+problem link-https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_scores = student_marks[query_name]
+    print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
