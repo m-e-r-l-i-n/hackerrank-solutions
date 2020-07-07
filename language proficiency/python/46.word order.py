29bdaa03e2aba112a8cda93e92f74f1bc8fb6eec
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-07-07 16:19:28 +0530

    Create 46.word order.py

---
+++ language proficiency/python/46.word order.py
@@ -0,0 +1,13 @@
+problem link-https://www.hackerrank.com/challenges/word-order/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+words=OrderedDict()
+
+for _ in range(int(input())):
+    word=input()
+    words.setdefault(word,0)
+    words[word]+=1
+   
+print(len(words))
+print(*words.values())
