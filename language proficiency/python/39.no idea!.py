ae54a9911720d2354884b1f819056f3d1f0bf1d8
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-07-07 16:19:58 +0530

    Create 39.no idea!.py

---
+++ language proficiency/python/39.no idea!.py
@@ -0,0 +1,8 @@
+problem link-
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,m=input().split()
+ar=input().split()
+a=set(input().split())
+b=set(input().split())
+print(sum([(i in a)-(i in b) for i in ar]))
